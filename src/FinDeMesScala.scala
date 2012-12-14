415b16ab71e675a45db33ea461e179d210bf1d39
danibuiza <danibuiza@gmail.com>
2012-12-14 13:42:24 +0100

    initial commit

---
+++ src/FinDeMesScala.scala
@@ -0,0 +1,18 @@
+import scala.io.Source
+
+object FinDeMesScala extends App {
+
+ 
+  var johanna: Double = 0.0
+  var dani: Double = 0.0
+  for (line <- Source.fromFile("C:\\findemes\\input.txt").getLines()) {
+
+    val pieces = line.split(" ")
+    if ("Johanna".equalsIgnoreCase(pieces(0))) {
+      johanna = johanna + pieces(1).toDouble
+    } else if ("Dani".equalsIgnoreCase(pieces(0))) {
+      dani = dani + pieces(1).toDouble 
+    }
+  }
+  println("dani gasto " + dani + " y johanna " + johanna + " por lo que dani le debe a johanna " + (johanna - dani) / 2)
+}
